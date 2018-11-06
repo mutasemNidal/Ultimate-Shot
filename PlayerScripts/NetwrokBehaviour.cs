124e68237c808b70879083a4608eaa08bbc94c4c
MutasemNidal <mutasem.nidal@gmail.com>
2018-11-06 18:32:09 +0200

    first

---
+++ PlayerScripts/NetwrokBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetwrokBehaviour : NetworkBehaviour {
+    [SerializeField]
+    Behaviour[] ComToDisable;
+    void Start () {
+        if (!isLocalPlayer)
+        {
+            for(int i = 0; i < ComToDisable.Length; i++)
+            {
+                ComToDisable[i].enabled = false;
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
